4f76cf9f9464c0f3193682de187e1606f141c60d
Nemanja <nemadjo94@gmail.com>
2020-03-26 12:11:38 +0100

    Implemented withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => {
+  return (
+    <h1>Hats page</h1>
+  )
+}
 
 function App() {
   return (
-    <HomePage></HomePage>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
+    </div>
   );
 }
 
