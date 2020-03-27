4eab7bf7a8d1a6c140bcc09f46317ee9a5bf20f2
Nemanja <nemadjo94@gmail.com>
2020-03-27 12:09:13 +0100

    Added sign in page with components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './header.styles.scss';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/original.svg';
+import { auth } from '../../firebase/firebase.utils';
 
-const Header = () => {
+const Header = ({ currentUser }) => {
     return (
         <div className='header'>
             <Link className='logo-container' to='/' >
@@ -16,6 +17,11 @@ const Header = () => {
                 <Link className='option' to='/contact'>
                     CONTACT
                 </Link>
+                {
+                    currentUser 
+                    ? <Link className='option' onClick={() => auth.signOut()}>SIGN OUT</Link>
+                    : <Link className='option' to='/signin'>SIGN IN</Link>
+                }
             </div>
         </div>
     )
