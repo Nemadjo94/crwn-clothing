8392f1ff9702973a40a914cae1f8a39e0cc36fe2
Nemanja <nemadjo94@gmail.com>
2020-03-28 21:13:46 +0100

    Added shop and directory reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root.reducer';
+import { persistStore } from 'redux-persist';
 
 const middlewares = [logger]; // add all middleware options in this array
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares)) // this spreads all the values in the middleware
 
-export default store;
+const persistor = persistStore(store); // we use persistor to create stored version of our store
+
+export {store, persistor};
 
